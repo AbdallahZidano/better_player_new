5d833c35daa28b2d041230a3765194216d9018c8
Abdallah Zidano <eng.zidano@yahoo.com>
2023-09-30 01:08:38 +0300

    add custom speeds

--- lib/src/controls/better_player_controls_state.dart
+++ lib/src/controls/better_player_controls_state.dart
@@ -140,24 +140,37 @@ abstract class BetterPlayerControlsState<T extends StatefulWidget>
   }
 
   void _showSpeedChooserWidget() {
-    _showModalBottomSheet([
-      _buildSpeedRow(0.25),
-      _buildSpeedRow(0.5),
-      _buildSpeedRow(0.75),
-      _buildSpeedRow(1.0),
-      _buildSpeedRow(1.25),
-      _buildSpeedRow(1.5),
-      _buildSpeedRow(1.75),
-      _buildSpeedRow(2.0),
-      // _buildSpeedRow(2.25),
-      // _buildSpeedRow(2.50),
-      // _buildSpeedRow(2.75),
-      // _buildSpeedRow(3.0),
-      // _buildSpeedRow(3.25),
-      // _buildSpeedRow(3.50),
-      // _buildSpeedRow(3.75),
-      // _buildSpeedRow(4.0),
-    ]);
+    if (Platform.isAndroid) {
+      _showModalBottomSheet([
+        _buildSpeedRow(0.25),
+        _buildSpeedRow(0.5),
+        _buildSpeedRow(0.75),
+        _buildSpeedRow(1.0),
+        _buildSpeedRow(1.25),
+        _buildSpeedRow(1.5),
+        _buildSpeedRow(1.75),
+        _buildSpeedRow(2.0),
+        _buildSpeedRow(2.25),
+        _buildSpeedRow(2.50),
+        _buildSpeedRow(2.75),
+        _buildSpeedRow(3.0),
+        _buildSpeedRow(3.25),
+        _buildSpeedRow(3.50),
+        _buildSpeedRow(3.75),
+        _buildSpeedRow(4.0),
+      ]);
+    } else {
+      _showModalBottomSheet([
+        _buildSpeedRow(0.25),
+        _buildSpeedRow(0.5),
+        _buildSpeedRow(0.75),
+        _buildSpeedRow(1.0),
+        _buildSpeedRow(1.25),
+        _buildSpeedRow(1.5),
+        _buildSpeedRow(1.75),
+        _buildSpeedRow(2.0),
+      ]);
+    }
   }
 
   Widget _buildSpeedRow(double value) {
